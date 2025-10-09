0e070163b19fb478d989cbdd84f332ed507784d5
avk0004 <vric.vignesh04@gmail.com>
2025-10-09 18:56:18 +0530

    basic loop

---
+++ Pattern/src/loop/Loop1.java
@@ -0,0 +1,13 @@
+package loop;
+
+public class Loop1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =5;
+		for(int i=1;i<=5;i++) {
+			System.out.println(i);
+		}
+	}
+
+}
