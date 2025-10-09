c0cdec12b3fb2750c7271b82869496982abc7e77
avk0004 <vric.vignesh04@gmail.com>
2025-10-09 18:59:52 +0530

    loop

--- Pattern/src/loop/Loop1.java
+++ Pattern/src/loop/Loop1.java
@@ -5,8 +5,11 @@ public class Loop1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a =5;
+		int c =0;
 		for(int i=1;i<=5;i++) {
 			System.out.println(i);
+			c+=i;
+			System.out.println(c);
 		}
 	}
 
